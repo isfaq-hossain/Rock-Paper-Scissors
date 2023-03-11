4e78fa28a079e6c89c0d63ff0d7e03913e1970d0
Isfaq <sabbirhossainsh04@gmail.com>
2023-03-11 22:05:07 +0600

    Modified
    Added a simple winOrLose() function to reduce the repetitiveness of return statement of playRound()

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const strings = ['rock', 'paper', 'scissors'];
+let playerSelection = 'Rock';
+playerSelection = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
 
 //Returns a random string from the list of strings
 function getComputerChoice() {
@@ -9,23 +12,23 @@ function getComputerChoice() {
 
 //plays a round and returns win or lose statement
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
     if (playerSelection === computerSelection) {
         return 'Its a Tie!'
     } else if (playerSelection === 'rock') {
-        return (computerSelection === 'scissors' ? `You Win! ${playerSelection} beats ${computerSelection}`
-            : `You Lose! ${computerSelection} beats ${playerSelection}`);
+        return (computerSelection === 'scissors' ? winOrLose('win')
+            : winOrLose('lose'));
     } else if (playerSelection === 'paper') {
-        return (computerSelection === 'rock' ? `You Win! ${playerSelection} beats ${computerSelection}`
-            : `You Lose! ${computerSelection} beats ${playerSelection}`);
+        return (computerSelection === 'rock' ? winOrLose('win')
+            : winOrLose('lose'));
     } else if (playerSelection === 'scissors') {
-        return (computerSelection === 'paper' ? `You Win! ${playerSelection} beats ${computerSelection}`
-            : `You Lose! ${computerSelection} beats ${playerSelection}`);
+        return (computerSelection === 'paper' ? winOrLose('win')
+            : winOrLose('lose'));
     }
 
 }
 
-const playerSelection = 'Rock';
-const computerSelection = getComputerChoice();
+function winOrLose(str) {
+    return (str === 'win' ? `You Win! ${playerSelection} beats ${computerSelection} `
+        : `You Lose! ${computerSelection} beats ${playerSelection} `)
+}
 console.log(playRound(playerSelection, computerSelection))

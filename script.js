2bf7a83f2ceebe92a5edcf226c37dca6675d0355
Isfaq <sabbirhossainsh04@gmail.com>
2023-03-11 21:46:11 +0600

    Add playRound() feature
    The playRound() function receives to parameter,  playerSelection and computerSelection.
    Then this function determines winner or Tie and returns that with statement

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const strings = ['Rock', 'Paper', 'Scissors'];
+const strings = ['rock', 'paper', 'scissors'];
 
+//Returns a random string from the list of strings
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
     let randomString = strings[randomIndex];
     return randomString;
 }
 
-console.log(getComputerChoice())
+//plays a round and returns win or lose statement
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return 'Its a Tie!'
+    } else if (playerSelection === 'rock') {
+        return (computerSelection === 'scissors' ? `You Win! ${playerSelection} beats ${computerSelection}`
+            : `You Lose! ${computerSelection} beats ${playerSelection}`);
+    } else if (playerSelection === 'paper') {
+        return (computerSelection === 'rock' ? `You Win! ${playerSelection} beats ${computerSelection}`
+            : `You Lose! ${computerSelection} beats ${playerSelection}`);
+    } else if (playerSelection === 'scissors') {
+        return (computerSelection === 'paper' ? `You Win! ${playerSelection} beats ${computerSelection}`
+            : `You Lose! ${computerSelection} beats ${playerSelection}`);
+    }
+
+}
+
+const playerSelection = 'Rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
